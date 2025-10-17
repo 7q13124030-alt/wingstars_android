65ac745c937077a7ea8395c7ff9d5fae9bf62df3
西安_李浩 <hao_li@newsoft.com.cn>
2025-10-17 11:44:31 +0800

    创建啦啦队项目

---
+++ app/src/main/java/com/company/wingstars/BaseActivity.kt
@@ -0,0 +1,7 @@
+package com.company.wingstars
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity :  AppCompatActivity(){
+
+}
---
+++ app/src/main/java/com/company/wingstars/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.company.wingstars
+
+import android.os.Bundle
+
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
