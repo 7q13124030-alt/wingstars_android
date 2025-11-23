2298f279c4713664c300dfb481798f4c00e26d18
Quang Linh <tqlinh30502@gmail.com>
2025-11-23 11:51:38 +0800

    [登入] 绘制登入、注册、忘记密码及欢迎页面

--- app/src/main/java/com/company/wingstars/main/MainActivity.kt
+++ app/src/main/java/com/company/wingstars/main/MainActivity.kt
@@ -10,7 +10,7 @@ import com.company.wingstars.databinding.ActivityMainBinding
 import com.wingstars.base.base.BaseActivity
 import com.wingstars.calendar.CalendarFragment
 import com.wingstars.count.CountFragment
-import com.wingstars.home.HomeFragment
+import com.wingstars.home.fragment.HomeFragment
 import com.wingstars.member.fragment.MemberFragment
 import com.wingstars.user.UserFragment
 
